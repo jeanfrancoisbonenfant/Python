fe7c9ea4ce250702a4fe4fdbe5b851ad86642b12
Jean-Francois Bonenfant <up4aparty_24_7@hotmail.com>
2022-03-06 16:21:32 -0700

    Merge laptop

---
+++ chapter_4/looping through a slice.py
@@ -0,0 +1,7 @@
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+
+print('Here is the 3 first players of my team:')
+
+#loop using only partial list
+for player in players[:3]:
+    print(player)
---
+++ chapter_5/exercice 5-7.py
@@ -0,0 +1,16 @@
+favorite_fruit = ["mango", "pineapple", "orange", 'banana', 'cherry']
+
+if "orange" in favorite_fruit:
+    print("I love them too")
+    
+if "apple" in favorite_fruit:
+    print("I am allergic")
+
+if "mango" in favorite_fruit:
+    print("Mango are SOOOOOO amazing!!!")
+
+if "cherry" in favorite_fruit:
+    print("Brigitte's favorite fruits")
+    
+if "banana" in favorite_fruit:
+    print("I like the shapeeeee of it ;)")
