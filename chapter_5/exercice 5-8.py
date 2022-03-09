f48638ed0c4e22b61feb31a1517a84f316c42955
Jean-Francois Bonenfant <up4aparty_24_7@hotmail.com>
2022-03-09 00:37:38 -0700

    Delete chapter_5 directory

--- chapter_5/exercice 5-8.py
+++
@@ -1,9 +0,0 @@
-username = ["admin", 'venoma', "bay", 'Yuki', "Baltazzar"]
-
-for user in username:
-    
-    if user == "admin":
-        print(f"Hello {user.title()}!, would you like to see a status report?")
-    else:
-    
-        print(f"Welcome {user.title()}, thank you for logging in again.")
