6b60dcbc31be4ef55140470ce98b1d77a1b98e74
Jean-Francois Bonenfant <up4aparty_24_7@hotmail.com>
2022-03-09 00:37:31 -0700

    Delete chapter_4 directory

--- chapter_4/Working Part list.py
+++
@@ -1,5 +0,0 @@
-#Slicing list 
-players = ['charles', 'martina', 'michael', 'florence', 'eli']
-print(players[0:3])
-
-print(players[1:])
--- chapter_4/exercices 4-3.py
+++
@@ -1,5 +0,0 @@
-#Use a for loop to print the numbers from 1 to 20,
-#inclusive.
-
-numbers = [value for value in range(1,21)]
-print(numbers)
