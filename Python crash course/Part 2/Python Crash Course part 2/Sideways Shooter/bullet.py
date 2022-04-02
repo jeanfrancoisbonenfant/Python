7a9604aaa6698b2b85ca9bb4f675a6a41e11c3f6
Jean-Francois Bonenfant <up4aparty_24_7@hotmail.com>
2022-04-01 22:30:28 -0600

    exercice 12-6 Sideways shooter

--- Python crash course/Part 2/Python Crash Course part 2/Sideways Shooter/bullet.py
+++ Python crash course/Part 2/Python Crash Course part 2/Sideways Shooter/bullet.py
@@ -4,28 +4,29 @@ from pygame.sprite import Sprite
 class Bullet(Sprite):
     """ A class to manage bullets fired from the ship. """
 
-    def __init__(self, ai_game):
+    def __init__(self, ss_game):
         """ Create a bullet object at the ship's position. """
         super().__init__()
-        self.screen = ai_game.screen
-        self.settings = ai_game.settings
+        self.screen = ss_game.screen
+        self.settings = ss_game.settings
         self.color = self.settings.bullet_color
+        self.screen_rect = ss_game.screen.get_rect()
 
-        #create a bullet rect at (0 , 0) and then set correct postion.
+        # Create a bullet rect at (0 , 0) and then set correct position.
         self.rect = pygame.Rect(0, 0, self.settings.bullet_width,
                                 self.settings.bullet_height)
-        self.rect.midtop = ai_game.ship.rect.midtop
+        self.rect.midright = ss_game.ship.rect.midright
 
         # Store the bullet's position as a decimal value.
-        self.y = float(self.rect.y)
+        self.x = float(self.rect.x)
 
     def update(self):
-        """ Move the bullet up the screen. """
+        """ Move the bullet to the right of screen. """
         # Update the decimal position of the bullet.
-        self.y -= self.settings.bullet_speed
-        # Update the rect position.
-        self.rect.y = self.y
+        self.x += self.settings.bullet_speed
+        # Update the rect positions.
+        self.rect.x = self.x
 
     def draw_bullet(self):
-        """ Draw the bullet to the screen. """
+        """ Draw the bullet t the screen. """
         pygame.draw.rect(self.screen, self.color, self.rect)
