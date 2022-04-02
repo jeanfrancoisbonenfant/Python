b3ab0353841cc8ed6ed75124b434d88239f1b74d
jeanfrancoisbonenfant <up4aparty_24_7@hotmail.com>
2022-04-01 19:50:43 -0600

    rocket man update

--- Python crash course/Part 2/Python Crash Course part 2/Rocket man/rocket.py
+++ Python crash course/Part 2/Python Crash Course part 2/Rocket man/rocket.py
@@ -15,6 +15,26 @@ class Rocket:
         # Start each new rocket at the bottom center of screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
+        #Movement flag
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_foward = False
+        self.moving_back = False
+
+    def update(self):
+        """ Update thr rocket's position based on the movement flag"""
+        # Update rocket's rect positions.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.rect.x += 1
+
+        if self.moving_left and self.rect.left > 0:
+            self.rect.x -= 1
+
+        if self.moving_foward and self.rect.top > 0:
+            self.rect.y -= 1
+
+        if self.moving_back and self.rect.bottom < self.screen_rect.bottom:
+            self.rect.y += 1
     def blitme(self):
         """ Draw the rocket at its current location."""
         self.screen.blit(self.image, self.rect)
--- Python crash course/Part 2/Python Crash Course part 2/Rocket man/rocket_man.py
+++ Python crash course/Part 2/Python Crash Course part 2/Rocket man/rocket_man.py
@@ -22,21 +22,51 @@ class RocketMan:
     def run_game(self):
         """ Start the main loop for the game. """
         while True:
-            for event in pygame.event.get():
+            self._check_event()
+            self.rocket.update()
+            self._update_screen()
+
+
+
+    def _check_event(self):
+        """ Respond to keypresses and mouse events. """
+        for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
                 elif event.type == pygame.KEYDOWN:
-                    """ Respond to keypresses. """
-                    if event.key == pygame.K_RIGHT:
-                        self.rocket.rect.x += 1
-                    elif event.key == pygame.K_LEFT:
-                        self.rocket.rect.x -= 1
-                    elif event.key == pygame.K_q:
-                        sys.exit()
+                    self._check_keydown_event(event)
+                elif event.type == pygame.KEYUP:
+                    self._check_keyup_event(event)
 
-            self.screen.fill(self.settings.bg_color)
-            self.rocket.blitme()
-            pygame.display.flip()
+    def _check_keydown_event(self,event):
+        """ Respond to keydown events. """
+        if event.key == pygame.K_RIGHT:
+            self.rocket.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.rocket.moving_left = True
+        elif event.key == pygame.K_UP:
+            self.rocket.moving_foward = True
+        elif event.key == pygame.K_DOWN:
+            self.rocket.moving_back = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_event(self,event):
+        """ Respond to keyup events. """
+        if event.key == pygame.K_RIGHT:
+            self.rocket.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.rocket.moving_left = False
+        elif event.key == pygame.K_UP:
+            self.rocket.moving_foward = False
+        elif event.key == pygame.K_DOWN:
+            self.rocket.moving_back = False
+
+    def _update_screen(self):
+        """ Update image on the screen and flip to the new screen. """
+        self.screen.fill(self.settings.bg_color)
+        self.rocket.blitme()
+        pygame.display.flip()
 
 if __name__ == '__main__':
     rm = RocketMan()
